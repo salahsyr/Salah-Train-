7ba916cb9544c33c1ae7f7dfb3564e826f920a8d
salahsyr <108382907+salahsyr@users.noreply.github.com>
2022-10-21 14:02:43 +0200

    update gojo.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['96891103640']
-global.premium = ['96891103640']
-global.ownernomer = '96891103640'
-global.ownername = '( الـمـطـور )'
-global.botname = '( بـوت غـوجـو )'
-global.footer = '( غـوجـو )'
+global.owner = ['31624789483']
+global.premium = ['31624789483']
+global.ownernomer = '31624789483'
+global.ownername = '( ترين )'
+global.botname = '( بـوت ترين )'
+global.footer = '(ترين )'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://chat.whatsapp.com/CKLmAGJo3iw5AvNM1KZee3'
 global.myweb = 'https://youtube.com/channel/UCPvrnWknQrgCSIeobXMP0Uw'
-global.packname = 'https://Wa.Me/+96891103640'
-global.author = '[ غـوجـو ]'
+global.packname = 'https://Wa.Me/+31624789483'
+global.author = '[ حقوق ترين ساما]'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
